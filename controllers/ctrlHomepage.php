320449b789bf2337deda0448854fe28e21bd4be3
sirine <sirine.b@codeur.online>
2018-07-20 16:06:23 +0200

    début de modif pour la page dashboard

--- controllers/ctrlHomepage.php
+++ controllers/ctrlHomepage.php
@@ -1,2 +1,13 @@
 <?php 
-echo("coucou");
+
+require("models/models.php");
+
+
+require 'vendor/autoload.php';
+$loader = new Twig_Loader_Filesystem('views');
+$twig = new Twig_Environment($loader, array(
+    'cache'=> false
+));
+
+$template = $twig->load('template.html.twig');
+echo $template->render(array(""));
