ed48ce8a08f13ae6db1fe23777bd0645206ca20b
Tom <martint033@gmail.com>
2018-07-20 14:21:31 +0200

    debut fonction anim

---
+++ models/fonctionAnimateur.php
@@ -0,0 +1,12 @@
+<?php
+
+require("../utils/db.php");
+
+function create($insertInto, $val){
+    global $db;
+    $response = $db->prepare("INSERT INTO :insertInto VALUES :val");
+    $response->bindParam(':insertInto', $insertInto);
+    $response->bindParam(':val', $val);
+    $response->execute();
+    return true; 
+}
