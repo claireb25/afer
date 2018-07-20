9e4810fd3a1e85a1c6495aa71497ab6640feab08
Tom <martint033@gmail.com>
2018-07-20 14:21:05 +0200

    debut fonctionAnimateur

--- index.php
+++ index.php
@@ -1,23 +1,23 @@
 <?php
-
-
     $path = '';
-
 
     if( isset( $_GET['controller'] ) ){
         $path = $_GET['controller']; 
     }
     
-    $path = ( $path== '' )? 'afer-back' : $path ;
-
-
+    $path = ( $path== '' )? 'afer-back' : $path;
 
     switch ($path) {
         case "afer-back":
             require('controllers/ctrlLoginpage.php');
             break;
+
         case "statutanimateur":
             require('controllers/ctrlStatutAnimateur.php');
+            break;
+
+        case "fonctionanimateur":
+            require('controllers/ctrlFonctionAnimateur.php');
             break;
         default :
             require('controllers/ctrl404.php');
