96878ba8713d514722200ce6e39e818b23a48fb3
Tom <martint033@gmail.com>
2018-07-20 16:56:12 +0200

    fonction animateur complete

--- models/fonctionAnimateur.php
+++ models/fonctionAnimateur.php
@@ -16,10 +16,28 @@ function listOne($tables, $id){
     return $response->fetch(PDO::FETCH_ASSOC);
 }
 
+function edit($nouvelleValeur, $id){
+    global $db;
+    $response = $db->prepare("UPDATE fonction_animateur SET fonction_nom = :nouvelleValeur WHERE id = :id");
+    $response->bindParam(':nouvelleValeur', $nouvelleValeur, PDO::PARAM_STR);
+    $response->bindParam(':id', $id, PDO::PARAM_INT);
+    $response->execute();
+    return true; 
+}
+
 function create($tables, $val){
     global $db;
     $response = $db->prepare("INSERT INTO $tables VALUES(:val)");
     $response->bindParam(':val', $val, PDO::PARAM_STR);
+    $response->execute();
+    return true; 
+}
+
+function delete($id){
+    global $db;
+    $response = $db->prepare("DELETE FROM fonction_animateur
+    WHERE id = :id");
+    $response->bindParam(':id', $id, PDO::PARAM_INT);
     $response->execute();
     return true; 
 }
