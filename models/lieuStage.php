4fb6e7ef9c015fd56015dd9e976ff7f7382bd354
Tom <martint033@gmail.com>
2018-07-24 09:47:47 +0200

    defraiement propre et lieu stage OK

--- models/lieuStage.php
+++ models/lieuStage.php
@@ -1,35 +1,49 @@
 <?php 
 require_once("utils/db.php");
 // NEW
-function create($valeur){
+function create($lieuNom, $etablissementNom, $adresse, $codePostal, $commune, $tel, $latitude, $longitude, $divers){
     global $db;
-    $response = $db->prepare("INSERT INTO defraiement VALUES ($valeur)");
+    $response = $db->prepare("INSERT INTO lieu_stage(lieu_nom, etablissement_nom, adresse, code_postal, commune, tel, latitude, longitude, divers) VALUES(:lieuNom, :etablissementNom, :adresse, :codePostal, :commune, :tel, :latitude, :longitude, :divers)");
+    $response->bindParam(':lieuNom', $lieuNom, PDO::PARAM_STR);
+    $response->bindParam(':etablissementNom', $etablissementNom, PDO::PARAM_STR);
+    $response->bindParam(':adresse', $adresse, PDO::PARAM_STR);
+    $response->bindParam(':codePostal', $codePostal, PDO::PARAM_STR);
+    $response->bindParam(':commune', $commune, PDO::PARAM_STR);
+    $response->bindParam(':tel', $tel, PDO::PARAM_STR);
+    $response->bindParam(':latitude', $latitude, PDO::PARAM_STR);
+    $response->bindParam(':longitude', $longitude, PDO::PARAM_STR);
+    $response->bindParam(':divers', $divers, PDO::PARAM_STR);
     $response->execute();
     return true; 
 }
 //LIST
 function listAll(){
     global $db;
-    $response = $db->prepare("SELECT id, repas, km_ar FROM defraiement");
+    $response = $db->prepare("SELECT * FROM lieu_stage");
     $response->execute();
     return $response->fetchAll(PDO::FETCH_ASSOC);
 }
 //EDIT
 function getOne($id){
     global $db;
-    $response = $db->prepare("SELECT id, repas, km_ar FROM defraiement WHERE defraiement.id = :id");
+    $response = $db->prepare("SELECT * FROM lieu_stage WHERE lieu_stage.id = :id");
     $response->bindParam(':id', $id, PDO::PARAM_INT);
     $response->execute();
     return $response->fetch(PDO::FETCH_ASSOC);
 }
-function edit($colonne, $valeur, $id){
+function edit($lieuNom, $etablissementNom, $adresse, $codePostal, $commune, $tel, $latitude, $longitude, $divers, $id){
     global $db;
-    $response = $db->prepare("UPDATE defraiement
-    SET $colonne = $valeur 
-    WHERE id = $id");
-    // $response->bindParam(':colonne', $colonne, PDO::PARAM_STR);
-    // $response->bindParam(':valeur', $valeur, PDO::PARAM_STR);
-    // $response->bindParam(':id', $id, PDO::PARAM_INT);
+    $response = $db->prepare("UPDATE lieu_stage SET lieu_nom = :lieuNom, etablissement_nom = :etablissementNom, adresse = :adresse, code_postal = :codePostal, commune = :commune, tel = :tel, latitude = :latitude, longitude = :longitude, divers = :divers WHERE id = :id");
+    $response->bindParam(':lieuNom', $lieuNom, PDO::PARAM_STR);
+    $response->bindParam(':etablissementNom', $etablissementNom, PDO::PARAM_STR);
+    $response->bindParam(':adresse', $adresse, PDO::PARAM_STR);
+    $response->bindParam(':codePostal', $codePostal, PDO::PARAM_STR);
+    $response->bindParam(':commune', $commune, PDO::PARAM_STR);
+    $response->bindParam(':tel', $tel, PDO::PARAM_STR);
+    $response->bindParam(':latitude', $latitude, PDO::PARAM_STR);
+    $response->bindParam(':longitude', $longitude, PDO::PARAM_STR);
+    $response->bindParam(':divers', $divers, PDO::PARAM_STR);;
+    $response->bindParam(':id', $id, PDO::PARAM_INT);
     $response->execute();
     return true; 
 }
@@ -37,7 +51,7 @@ function edit($colonne, $valeur, $id){
 //DELETE
 function delete($id){
     global $db;
-    $response = $db->prepare("DELETE FROM defraiement
+    $response = $db->prepare("DELETE FROM lieu_stage
     WHERE id = :id");
     $response->bindParam(':id', $id, PDO::PARAM_INT);
     $response->execute();
