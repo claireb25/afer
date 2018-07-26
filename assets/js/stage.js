bf22c1b4bceccd383c53b508eec4c8f40ef6308c
claireb25 <claire.bourgeois.cb@gmail.com>
2018-07-26 16:49:37 +0200

    crud et js lieux de stage (js ok crud non fini)

---
+++ assets/js/stage.js
@@ -0,0 +1,110 @@
+
+$(document).ready( function() {
+  
+    $('body').on("click", ".form div h3", function(){
+        console.log($(this).children('div'))
+      if ($(this).children('span').hasClass('close')) {
+        $(this).children('span').removeClass('close');
+      }
+      else {
+        $(this).children('span').addClass('close');
+      }
+      $(this).parent().children('div').slideToggle(250);
+    });
+
+});
+
+
+var inputLieuStage = document.getElementById('lieu_stage');
+inputLieuStage.addEventListener('keyup', function(e){
+  autoComplete();
+  
+});
+
+function autoComplete(){
+  var min_length = 1; // min caracters to display the autocomplete
+  var keyword = inputLieuStage.value ;
+  if (keyword.length >= min_length) {
+    $.ajax({
+      url: 'stage/query',
+      type: 'POST',
+      data: {keyword:keyword},
+      success:function(data){
+        data = JSON.parse(data);
+
+        var html = "";
+        // var etbls = "";
+        for (elem of data){
+          html += '<li data-id="'+elem.id+'">'+ elem.lieu_nom + '</li> <input id="etablissement_hidden" name="etablissement" type="hidden" value="'+elem.etablissement_nom+'">';
+          document.getElementById('lieu_stage_list').innerHTML = html;
+
+        
+         
+          
+        // $('#lieu_stage_list').show();
+        // // console.log($('#lieu_stage_list'))
+        // $('#lieu_stage_list').html(data.lieu_nom);
+        }
+        itemClicked(data);
+      }
+    });
+  } else {
+    $('#lieu_stage_list').hide();
+  }
+}
+
+
+function itemClicked(data){
+  listElem= document.getElementById('input_lieu'); 
+  listElem.addEventListener('click', function(e){ // listen to click on children of input_lieu
+    var newValue = e.target.innerText //
+    var idValue = e.target.getAttribute('data-id');
+   
+    var etablissement = "";
+    var adresse ="";
+    var code_postal ="";
+    var commune ="";
+    var tel = "";
+    var latitude = "";
+    var longitude = "";
+    var divers ="";
+
+    document.getElementById('lieu_stage').value = newValue; // change main input name
+    document.getElementById('lieu_stage_hidden').value = idValue; // change value of hidden input into chosen id number
+    
+    for (elements of data){ //if data element is the same as chosen element, change all the other inputs values 
+      if (elements.lieu_nom == e.target.innerText){
+        console.log(elements);
+        etablissement = elements.etablissement_nom;
+        document.getElementById('etablissement_nom').value = etablissement;
+
+        adresse = elements.adresse;
+        document.getElementById('adresse').value = adresse;
+
+        code_postal = elements.code_postal;
+        document.getElementById('code_postal').value = code_postal;
+
+        commune = elements.commune;
+        document.getElementById('commune').value = commune;
+
+        tel = elements.tel;
+        document.getElementById('tel').value = tel;
+
+        latitude = elements.latitude;
+        document.getElementById('latitude').value = latitude;
+
+        longitude = elements.longitude;
+        document.getElementById('longitude').value = longitude;
+
+        divers = elements.divers;
+        document.getElementById('divers').value = divers;
+      }
+     
+
+    }  
+  })
+
+}  
+ 
+
+
---
+++ models/stage.php
@@ -0,0 +1,90 @@
+<?php 
+require_once("utils/db.php");
+// NEW
+function create($lieu_stage, $stage_numero, $date_debut, $date_fin, $stage_hpo){
+    global $db;
+    $response = $db->prepare("INSERT INTO stage(lieu_stage_id_id, stage_numero, date, stage_hpo, date_fin) VALUES(:lieu_stage, :stage_numero, :date_debut, :stage_hpo, :date_fin)");
+    $response->bindParam(':lieu_stage', $lieu_stage, PDO::PARAM_INT);
+    $response->bindParam(':stage_numero', $stage_numero, PDO::PARAM_STR);
+    $response->bindParam(':date_debut', $date_debut, PDO::PARAM_STR);
+    $response->bindParam(':stage_hpo', $stage_hpo, PDO::PARAM_BOOL);
+    $response->bindParam(':date_fin', $date_fin, PDO::PARAM_STR);
+ 
+    $response->execute();
+    return true; 
+}
+//LIST
+function listAll(){
+    global $db;
+    $response = $db->prepare("SELECT stage.id, stage.stage_numero, lieu_stage.lieu_nom, stage_numero, date, stage_hpo, date_fin FROM `stage` INNER JOIN lieu_stage ON stage.lieu_stage_id_id = lieu_stage.id");
+    $response->execute();
+    return $response->fetchAll(PDO::FETCH_ASSOC);
+}
+function listLieux($keyword){
+    global $db;
+    $response = $db->prepare("SELECT * FROM lieu_stage WHERE lieu_nom LIKE :keyword");
+    $keyword = $keyword.'%';
+    $response->bindParam(':keyword', $keyword, PDO::PARAM_STR);
+   
+    $response->execute();
+    $response = $response->fetchAll(PDO::FETCH_ASSOC);
+    echo json_encode($response);
+    // foreach ($result as $rs) {
+    //     // put in bold the written text
+    //     $lieu_nom = str_replace($_POST['keyword'], '<b>'.$_POST['keyword'].'</b>', $rs['lieu_nom']);
+        
+    //     // add new option
+    //     echo '<li onclick="set_item(\''.str_replace("'", "\'", $rs['lieu_nom']).'\')">'.$lieu_nom.'</li>';
+    // }
+}
+
+
+
+
+// //EDIT
+// function getOne($id){
+//     global $db;
+//     $response = $db->prepare("SELECT animateur.id, animateur.civilite_id_id, civilite.nom, animateur.fonction_animateur_id_id, fonction_animateur.fonction_nom, animateur.statut_id_id, statut_animateur.status_nom, animateur.nom, prenom, gta, raison_sociale, adresse, code_postal, commune, region, tel_portable, tel_fixe, email, urssaf, siret, observations FROM animateur INNER JOIN civilite ON animateur.civilite_id_id = civilite.id INNER JOIN fonction_animateur ON animateur.fonction_animateur_id_id = fonction_animateur.id INNER JOIN statut_animateur ON statut_id_id = statut_animateur.id 
+//     WHERE animateur.id = :id");
+//     $response->bindParam(':id', $id, PDO::PARAM_INT);
+//     $response->execute();
+//     return $response->fetch(PDO::FETCH_ASSOC);
+// }
+// function edit($civilite, $nom, $prenom, $fonction, $statut, $gta, $raison_sociale, $adresse, $code_postal, $ville, $region, $tel_portable, $tel_fixe, $email, $urssaf, $siret, $observations, $id){
+//     global $db;
+//     $response = $db->prepare("UPDATE animateur SET civilite_id_id = :civilite, fonction_animateur_id_id = :fonction,statut_id_id = :statut, nom = :nom, prenom = :prenom, gta = :gta, raison_sociale = :raison_sociale, adresse = :adresse, code_postal = :code_postal, commune = :ville, region = :region, tel_portable = :tel_portable,tel_fixe = :tel_fixe, email = :email, urssaf = :urssaf, siret = :siret, observations = :observations WHERE animateur.id = :id");
+//     $response->bindParam(':civilite', $civilite, PDO::PARAM_INT);
+//     $response->bindParam(':nom', $nom, PDO::PARAM_STR);
+//     $response->bindParam(':prenom', $prenom, PDO::PARAM_STR);
+//     $response->bindParam(':fonction', $fonction, PDO::PARAM_INT);
+//     $response->bindParam(':statut', $statut, PDO::PARAM_INT);
+//     $response->bindParam(':gta', $gta, PDO::PARAM_BOOL);
+//     $response->bindParam(':raison_sociale', $raison_sociale, PDO::PARAM_STR);
+//     $response->bindParam(':adresse', $adresse, PDO::PARAM_STR);
+//     $response->bindParam(':code_postal', $code_postal, PDO::PARAM_STR);
+//     $response->bindParam(':ville', $ville, PDO::PARAM_STR);
+//     $response->bindParam(':region', $region, PDO::PARAM_STR);
+//     $response->bindParam(':tel_portable', $tel_portable, PDO::PARAM_STR);
+//     $response->bindParam(':tel_fixe', $tel_fixe, PDO::PARAM_STR);
+//     $response->bindParam(':email', $email, PDO::PARAM_STR);
+//     $response->bindParam(':urssaf', $urssaf, PDO::PARAM_STR);
+//     $response->bindParam(':siret', $siret, PDO::PARAM_STR);
+//     $response->bindParam(':observations', $observations, PDO::PARAM_STR);
+//     $response->bindParam(':id', $id, PDO::PARAM_INT);
+//     var_dump($civilite);
+//     var_dump($nom);
+//     $response->execute();
+//     return true; 
+// }
+
+
+
+//DELETE
+function delete($id){
+    global $db;
+    $response = $db->prepare("DELETE FROM animateur
+    WHERE id = :id");
+    $response->bindParam(':id', $id, PDO::PARAM_INT);
+    $response->execute();
+    return true; 
+}
