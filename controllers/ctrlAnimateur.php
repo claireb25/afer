38bc8f4b4f2a5db8640dc1f713bc03564d99a750
keolite <contact@keolite.fr>
2018-11-06 13:17:03 +0000

    [Animateur] Début controle du formulaire ajouter un nouvel animateur

--- controllers/ctrlAnimateur.php
+++ controllers/ctrlAnimateur.php
@@ -13,19 +13,95 @@ if (isset($_GET['action'])){
         case 'list':
             makeList();            
             break;
+
         case 'new':
-            if (count($_POST) > 0){
-                if (isset($_POST['gta'])){
-                    $_POST['gta'] = 1;
-                    addNew($_POST['civilite'], $_POST['nom'], $_POST['prenom'], $_POST['fonction'], $_POST['statut'], $_POST['gta'], $_POST['raison_sociale'], $_POST['adresse'], $_POST['code_postal'], $_POST['ville'], $_POST['region'], $_POST['tel_portable'], $_POST['tel_fixe'], $_POST['email'], $_POST['urssaf'], $_POST['siret'], $_POST['observations']);
-                } else {
-                    $_POST['gta'] = 0;
-                    addNew($_POST['civilite'], $_POST['nom'], $_POST['prenom'], $_POST['fonction'], $_POST['statut'], $_POST['gta'], $_POST['raison_sociale'], $_POST['adresse'], $_POST['code_postal'], $_POST['ville'], $_POST['region'], $_POST['tel_portable'], $_POST['tel_fixe'], $_POST['email'], $_POST['urssaf'], $_POST['siret'], $_POST['observations']);
-                    
+        if ( count( $_POST )  > 0 ){
+            $civilite = '';
+            $nom = '';
+            $prenom = '';
+            $adresse = '';
+            $code_postal = '';
+            $commune = '';
+            $region = '';
+            $gta = '';
+            $raison_sociale = '';
+            $fonction_animateur = '';
+            $statut_animateur = '';
+            $urssaf = '';
+            $siret = '';
+            $tel_portable = '';
+            $tel_fixe = '';
+            $email = '';
+            $observation = '';
+
+
+            if( isset( $_POST['tribunal_nom'] ) ){
+                if( !empty( $_POST['tribunal_nom'] ) ){
+                    $tribunal_nom = htmlentities( trim( $_POST['tribunal_nom'] ) );
                 }
-            } else {
-                showNew();
             }
+
+            if( isset( $_POST['adresse'] ) ){
+                if( !empty( $_POST['adresse'] ) ){
+                    $adresse = htmlentities( trim( $_POST['adresse'] ) );
+                }
+            }
+
+
+            if( isset( $_POST['code_postal'] ) ){
+                if( !empty( $_POST['code_postal'] ) ){
+                    $code_postal = htmlentities( trim( $_POST['code_postal'] ) );
+                }
+            }
+
+            if( isset( $_POST['commune'] ) ){
+                if( !empty( $_POST['commune'] ) ){
+                    $commune = htmlentities( trim( $_POST['commune'] ) );
+                }
+            }
+
+            if( isset( $_POST['autorite_tribunal'] ) ){
+                if( !empty( $_POST['autorite_tribunal'] ) ){
+                    $autorite_tribunal = htmlentities( trim( $_POST['autorite_tribunal'] ) );
+                    $autorite_tribunal = (int) $autorite_tribunal;
+                }
+            }
+
+            if( isset( $_POST['service_tribunal'] ) ){
+                if( !empty( $_POST['service_tribunal'] ) ){
+                    $service_tribunal = htmlentities( trim( $_POST['service_tribunal'] ) );
+                    $service_tribunal = (int) $service_tribunal;
+                }
+            }
+
+
+            
+
+            if( $autorite_tribunal == "" ){
+                $autorite_tribunal = null;
+            }
+
+            if( $service_tribunal == "" ){
+                $service_tribunal = null;
+            }
+
+
+            
+            
+            
+            
+            $reponse = (int) getCountTribunal( $tribunal_nom,  $adresse, $code_postal, $commune, $autorite_tribunal, $service_tribunal);
+            
+            
+            if( $reponse === 0 ){                   
+                addNew( $tribunal_nom, $autorite_tribunal, $service_tribunal, $adresse, $code_postal, $commune    );
+            }else{
+                showExist( $tribunal_nom,  $adresse, $code_postal, $commune, $autorite_tribunal, $service_tribunal );
+            }
+            
+        } else {
+            showNew();
+        }
             break; 
         case 'edit':
             if (count($_POST) > 0){   
